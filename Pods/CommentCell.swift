0179f4034c787aefa7de201e8c25a2f717dc8e13
Pablo  Pena <pabloj.pena@Pablos-Air.lan>
2021-10-11 17:37:36 -0400

    Unit 6 req

--- Pods/CommentCell.swift
+++ Pods/CommentCell.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class CommentCell: UITableViewCell {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var CommentLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
